66c9e4931b79a1de372b8d9da133610625331116
Jack Kamaryt <jkamaryt46@gmail.com>
2022-12-15 15:00:58 -0600

    Initial Commit

---
+++ RPS/RPSApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSApp.swift
+//  RPS
+//
+//  Created by Jack Kamaryt on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct RPSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
